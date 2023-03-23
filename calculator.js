cb841c5841b3e682221722ae1320feb9b0d29a2b
hergottwilliam <hergottwilliam@gmail.com>
2023-03-23 19:08:34 -0400

    added pseudocode to js file

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@
 // all operators are class = "btn opr"
 // = : class = "btn eql"
 // AC: class = "btn clr"
-// all buttons have an id matching the buttons text content
+// all buttons have an id matching the buttons text content, except multiplication (id = *, text content = x)
 
 // all math functions
 function add(num1, num2){
@@ -23,6 +23,13 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
+// only two "levels" of bedmas
+const bedmasFirst = ["*", "/"];
+const bedmasSecond = ["+", "-"];
+
+let currentNumber = "";
+let currentCalculation = [];
+ 
 // match operator symbol to appropriate function
 let operatorFunction = {
     "+" : add,
@@ -37,7 +44,23 @@ function operate(operator, num1, num2){
 }
 
 // create a function that takes an array and does bedmas on it, using operate(), returning the result
-function bedmas()
+function bedmas(array){
+    while (array.length > 1){
+        for (let i = 0; i < array.length; i++){
+            if (array[i] in bedmasFirst){
+                // calculate and replace
+            }
+            else if (array[i] in bedmasSecond){
+                // calculate and replace
+
+            }
+            else {
+                continue;
+            }
+        }
+    }
+
+}
 
 let display = document.querySelector(".display");
 
@@ -46,6 +69,10 @@ const buttons = document.querySelectorAll(".btn");
 buttons.forEach((button) => {
 
     button.addEventListener("click", function(e){
-        console.log(e.target.id);
+        // if number is clicked, add button.id to "current number"
+        // if operator is clicked, end current number and add it to current calculation array
+        // if = is cliked: bedmas(currentCalculation)
+        // if AC is clicked, reset everything
+        // update display everytime button is clicked
     });
 });
